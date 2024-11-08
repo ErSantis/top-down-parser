1ffae1dc99a165e3bfcd39fe9f642ac299f475ac
Elkin <ersantis@uninorte.edu.co>
2024-11-08 12:45:55 -0500

    Update Header component: change GitHub link and add GitHub icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,8 +4,10 @@ import '../styles/Header.css'; // Asegúrate de crear este archivo CSS
 const Header: React.FC = () => {
     return (
         <header className="header">
-            <a href="https://github.com/tu-usuario" className="github-link" target="_blank" rel="noopener noreferrer">
-                GitHub
+            <a href="https://github.com/ErSantis/top-down-parser.git" className="github-link" target="_blank" rel="noopener noreferrer">
+                <svg className="github-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+                    <path d="M12 0C5.37 0 0 5.37 0 12c0 5.3 3.438 9.8 8.205 11.387.6.113.82-.263.82-.583 0-.288-.01-1.05-.015-2.06-3.338.725-4.042-1.61-4.042-1.61-.546-1.387-1.333-1.757-1.333-1.757-1.09-.745.083-.73.083-.73 1.205.085 1.84 1.237 1.84 1.237 1.07 1.835 2.807 1.305 3.492.997.108-.775.42-1.305.763-1.605-2.665-.3-5.467-1.332-5.467-5.93 0-1.31.467-2.38 1.235-3.22-.125-.303-.535-1.523.115-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.4 3-.405 1.02.005 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.655 1.653.245 2.873.12 3.176.77.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.43.37.815 1.1.815 2.22 0 1.605-.015 2.895-.015 3.285 0 .32.215.7.825.58C20.565 21.795 24 17.295 24 12c0-6.63-5.37-12-12-12z"/>
+                </svg>
             </a>
             <h1 className="app-title">Top-down Parser</h1>
         </header>
